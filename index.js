437d7b1cb70d21dafa9f0c2e62259902a88bc5e5
violetavillamizara <villamizaravioleta@gmail.com>
2023-09-29 07:13:23 -0500

    refactor: :recycle: validations, PUT, balance
    all code improve

--- index.js
+++ index.js
@@ -1,125 +1,134 @@
-const uri="https://650c15b747af3fd22f67006b.mockapi.io/cuentaIE";
-const form=document.querySelector("form");
+const uri = "https://651673ef09e3260018c9c8d8.mockapi.io/cuentaIE";
+const form = document.querySelector("form");
 
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const datos = Object.fromEntries(new FormData(e.target));
+  datos.monto = typeof datos.monto === "string" ? Number(datos.monto) : 0;
+  console.log(datos);
 
-form.addEventListener("submit", async (e)=>{
-    e.preventDefault();
-    const datos=Object.fromEntries(new FormData(e.target));
-    datos.monto=(typeof datos.monto==="string") ? Number(datos.monto) : 0;
-    console.log(datos);
-    let config ={
-        method:"POST",
-        headers:{"content-type":"application/json"},
-        body:JSON.stringify(datos)
-    }
-    let res = await (await fetch(uri, config)).json();
-    console.log(res)
+  let config = {
+    method: "POST",
+    headers: { "content-type": "application/json" },
+    body: JSON.stringify(datos),
+  };
+
+  let res = await fetch(uri, config);
+  if (res.ok) {
+    console.log("Registro exitoso");
     window.location.reload();
+  } else {
+    console.error("Error al agregar el registro");
+  }
 });
 
+addEventListener("DOMContentLoaded", async () => {
+  const tabla = document.querySelector("table");
+  const tbody = tabla.querySelector("tbody");
 
-addEventListener("DOMContentLoaded", async()=>{
-    const tabla = document.querySelector("table"); 
+  const res = await fetch(uri);
+  const data = await res.json();
 
-    const tbody = tabla.querySelector("tbody");
-    const res = await(await fetch(uri)).json();
+  data.forEach((element) => {
+    const tr = `
+      <tr>
+        <td>${element.id}</td>
+        <td>${element.caja}</td>
+        <td>${element.monto}</td>
+        <td>
+          <button id="${element.id}" class="edit">Edit</button>
+          <button id="${element.id}" class="delet">Delete</button>
+        </td>
+      </tr>
+    `;
+    tbody.insertAdjacentHTML("beforeend", tr);
+  });
 
-    console.log(res);
-
-    res.forEach((element) => {
-        console.log(element)
-        const tr=`
-            <tr>
-                <td id="uno" >${element.id}</td>
-                <td id="dos" >${element.caja}</td>
-                <td id="tres" >${element.monto}</td>
-                <td>
-                <button id="${element.id}" class="edit">Edit</button>
-                <button id="${element.id}" class="delet">Delete</button>
-                </td>
-            </tr>
-        `;
-        tbody.insertAdjacentHTML("beforeend", tr);
-    });
-
-    delet();
-    // const btnEdit = document.querySelectorAll(".edit");
-    // btnEdit.addEventListener("click", edit())? edit():{};
-    // balance();
-    balance();
+  delet();
+  edit();
+  balance();
 });
 
-
-
-const delet = async ()=>{
-    const btnDelete = document.querySelectorAll(".delet");
-    btnDelete.forEach((element)=>{
-        
-        element.addEventListener("click", async(e)=>{
-            const id = e.target.getAttribute("id");
-            let config ={
-                method:"DELETE"
-            }
-            await fetch(uri + "/" + id, config);
-            window.location.reload();
-        });
-        balance();
+const delet = () => {
+  const btnDelete = document.querySelectorAll(".delet");
+  btnDelete.forEach((element) => {
+    element.addEventListener("click", async (e) => {
+      const id = e.target.getAttribute("id");
+      let config = {
+        method: "DELETE",
+      };
+      let res = await fetch(uri + "/" + id, config);
+      if (res.ok) {
+        console.log("Registro eliminado");
+        window.location.reload();
+      } else {
+        console.error("Error al eliminar el registro");
+      }
     });
+  });
 };
 
+const edit = () => {
+  const btnEdit = document.querySelectorAll(".edit");
+  btnEdit.forEach((element) => {
+    element.addEventListener("click", (e) => {
+      const row = element.closest("tr");
+      const id = row.querySelector("td").textContent;
+      const tipo = row.querySelector("td:nth-child(2)").textContent;
+      const valor = row.querySelector("td:nth-child(3)").textContent;
 
-const edit = async ()=>{
-    const btnEdit = document.querySelectorAll(".edit");
-    btnEdit.forEach((element)=>{
+      document.querySelector("#valor").value = valor;
+      document.querySelector(`input[value="${tipo}"]`).checked = true;
 
-    element.addEventListener("click", async(e)=>{
-        const row =element.closest("tr");
-        console.log(row);
-        const id = row.querySelector("#uno");
-        const tipo = row.querySelector("#dos");
-        const valor = row.querySelector("#tres");
-        console.log(id,tipo,valor);
-
-        document.querySelector("#valor").value=valor;
-        document.querySelector('input[value="${tipo}"]').checked=true;
-
-        const editBtn=document.querySelector("button");
-        editBtn.textContent="update";
-
-        subir();
-    })
-});
+      const editBtn = document.querySelector("button[type='submit']");
+      editBtn.textContent = "Update";
+      editBtn.setAttribute("data-edit-id", id);
+    });
+  });
 };
 
-const subir= async()=>{
-    let config ={
-        method:"PUT",
-        headers:{"content-type":"application/json"},
-        body: JSON.stringify()
-    }
-    let res = await (await fetch(uri + "/" + id, config)).json();
-}
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const editId = e.target.getAttribute("data-edit-id");
+  if (editId) {
+    subir(editId);
+  }
+});
 
-// const balance= async()=>{
-//     let total=0;
-//     const valor=document.querySelectorAll("#valor");
-//     const input=document.querySelectorAll('[name="caja"]');
-//     let actual=(input===value==="ingreso")?total+=valor:total-=valor;
-//     const span=document.querySelector("span");
-//     span.innerText=total;
-// };
+const subir = async (id) => {
+  const valor = document.querySelector("#valor").value;
+  const tipo = document.querySelector('input[name="caja"]:checked').value;
 
-const balance = async () => {
-    let total = 0;
-    const valor = parseFloat(document.querySelectorAll("#valor").value);
-    const input = document.querySelectorAll('[name="caja"]').value;
-  
+  let config = {
+    method: "PUT",
+    headers: { "content-type": "application/json" },
+    body: JSON.stringify({ monto: Number(valor), caja: tipo }),
+  };
+
+  let res = await fetch(uri + "/" + id, config);
+  if (res.ok) {
+    console.log("Registro actualizado");
+    window.location.reload();
+  } else {
+    console.error("Error al actualizar el registro");
+  }
+};
+
+const balance = () => {
+  let total = 0;
+  const valorInput = document.querySelector("#valor");
+  const valor = parseFloat(valorInput.value);
+  const input = document.querySelector('input[name="caja"]:checked').value;
+
+  if (!isNaN(valor)) {
     if (input === "ingreso") {
       total += valor;
     } else if (input === "egreso") {
       total -= valor;
     }
-  
-    const span = document.querySelector("span");
-    span.innerText = total;
-  };
+  }
+
+  const span = document.querySelector("h3 span");
+  span.innerText = total;
+};
+
